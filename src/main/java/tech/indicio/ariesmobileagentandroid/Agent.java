8c6596d0163f5b50796942ea39f81633fbed0c38
Patrick Kenyon <treek.kenyon@gmail.com>
2021-08-03 12:56:07 -0600

    Connected libindy
    Signed-off-by: Patrick Kenyon <treek.kenyon@gmail.com>

--- src/main/java/tech/indicio/ariesmobileagentandroid/Agent.java
+++ src/main/java/tech/indicio/ariesmobileagentandroid/Agent.java
@@ -1,7 +1,6 @@
 package tech.indicio.ariesmobileagentandroid;
 
 
-import android.system.ErrnoException;
 import android.util.Log;
 import org.hyperledger.indy.sdk.wallet.Wallet;
 
@@ -9,11 +8,23 @@ public class Agent {
     private static final String TAG = "AMAA-Agent";
 
     public Agent(){
-        Log.d(TAG, "Here is a message");
         try{
-            Wallet.createWallet("{'id':'abc'}", "{'key': '123'}");
+            Log.d(TAG, "Loading indy");
+            System.loadLibrary("indy");
+            Log.d(TAG, "Indy loaded, creating wallet...");
+
+            Wallet.createWallet("{\"id\":\"ioahsfajklshfkj1212\"}", "{\"key\": \"123\"}").get();
+            Log.d(TAG, "Wallet created");
+            Wallet wallet = Wallet.openWallet("{\"id\":\"ioahsfajklshfkj\"}", "{\"key\": \"123\"}").get();
+            Log.d(TAG, "Wallet opened");
         }catch(Exception e){
-            e.printStackTrace();
+            IndySdkRejectResponse rejectResponse = new IndySdkRejectResponse(e);
+            String code = rejectResponse.getCode();
+            String json = rejectResponse.toJson();
+            Log.e(TAG, "INDY ERROR");
+            Log.e(TAG, code);
+            Log.e(TAG, json);
+            Log.e(TAG, e.getMessage());
         }
 
     }
